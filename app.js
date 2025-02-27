6711f22857537469f7913839e8f6770876c95d46
Bhagyashri-Mandale <you@example.com>
2025-02-27 14:27:58 +0530

    course and guest lecturer module added

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require ('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/ERP_System");
-
+// mongoose.connect("mongodb://127.0.0.1:27017/ERP_System");
+mongoose.connect("mongodb+srv://Bhagyashri:55555@cluster1.uupr7.mongodb.net/ERP_System");
 const app = express();
 const Technology_route = require('./Technology/Technology_route');
 const University_route = require('./Univercity/University_route');
 const College_route = require('./College/College_route');
 const City_route = require('./City/City_route');
+const course_Route = require('./Courses/course_Route');
+const guestlecturer_Route = require('./Guest_lecturer/guestlecturer_Route');
 app.use(express.json())
 app.use(cors());
 
@@ -16,7 +18,8 @@ app.use('/', Technology_route)
 app.use('/', University_route)
 app.use('/', College_route)
 app.use('/',City_route)
-
+app.use('/',course_Route)
+app.use('/',guestlecturer_Route )
 app.get(('/'),(req,res) =>
     res.send("database canected ERP system"));
 
